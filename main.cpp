b1396ea1798ff9af9710576db8b90d54ea289745
Amitgb14 <amitg.b14@gmail.com>
2017-05-15 18:55:26 +0530

    Add cd command

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ int main()
 	con = m.connect();
 	std::cout << "Connected!\n";
 	
+	char useDB = 0;
 	
 	for(;;)
 	{
@@ -26,18 +27,37 @@ int main()
 		std::string query;
 
 		std::cout << "SQL>";
-		std::cin >> query;
+		std::getline(std::cin, query);
 
 
-		char execute = 0;		
+		char execute = 0;
+		std::cout << query << "\n";		
 		
-		if (query.compare("ls") == 0)
+		if(query.compare("ls") == 0)
 		{
-			query = "SHOW DATABASES;";
+			if(useDB == 0)
+				query = "SHOW DATABASES;";
+			else
+				query = "SHOW Tables;";
 			execute = 1;
 		}
     
-		else if (query.compare("exit") == 0)
+		else if(query.find("cd ") != std::string::npos)
+		{
+			size_t found = query.find("cd ");
+			std::string value = query.substr(found+3);
+			if (value.compare("..") == 0)
+				useDB = 0;
+			else
+				useDB = 1;
+				
+			if(useDB == 1)
+				query = "use "+ value + ";";
+				execute = 1;
+			
+		}
+    
+		else if(query.compare("exit") == 0)
 		{
 			std::cout << "\nBye!\n";
 			break;
@@ -45,22 +65,24 @@ int main()
 
 		if(execute)
 		{
-		   	result = m.query(con, query);
+			std::cout << "Query: " << query << "\n";
 			std::cout << "\nOutput!\n";
 			std::cout << "-----------------------------------------------!\n";
 
-		    	int num_fields = mysql_num_fields(result);
-
-		    	MYSQL_ROW row;
-		      
-		    	while ((row = mysql_fetch_row(result))) 
-		    	{ 
-				for(int i = 0; i < num_fields; i++) 
-			  	{ 
-			      		std::cout << row[i]; 
-			  	} 
-				std::cout << "\n"; 
-		    	}
+			if(result != 0)
+			{
+				int num_fields = mysql_num_fields(result);
+			    	MYSQL_ROW row;
+			      
+			    	while ((row = mysql_fetch_row(result))) 
+			    	{ 
+					for(int i = 0; i < num_fields; i++) 
+				  	{ 
+				      		std::cout << row[i]; 
+				  	} 
+					std::cout << "\n"; 
+			    	}
+			}
 		}
   
     	}
